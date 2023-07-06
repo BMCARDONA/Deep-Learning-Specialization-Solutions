2e1b1dcce553cee5f0fbced253f60f9bc8fc87ca
Bradley <bradleymathematics@gmail.com>
2023-07-06 15:47:56 -0400

    add solution

---
+++ neural_networks_and_deep_learning/module_2/python_basics_with_numpy/5_normalize_rows.py
@@ -0,0 +1,24 @@
+# GRADED FUNCTION: normalize_rows
+
+def normalize_rows(x):
+    """
+    Implement a function that normalizes each row of the matrix x (to have unit length).
+    
+    Argument:
+    x -- A numpy matrix of shape (n, m)
+    
+    Returns:
+    x -- The normalized (by row) numpy matrix. You are allowed to modify x.
+    """
+    
+    #(≈ 2 lines of code)
+    # Compute x_norm as the norm 2 of x. Use np.linalg.norm(..., ord = 2, axis = ..., keepdims = True)
+    # x_norm =
+    # Divide x by its norm.
+    # x =
+    # YOUR CODE STARTS HERE
+    x_norm = np.linalg.norm(x, ord=2, axis=1, keepdims=True)
+    x = x / x_norm
+    # YOUR CODE ENDS HERE
+
+    return x
