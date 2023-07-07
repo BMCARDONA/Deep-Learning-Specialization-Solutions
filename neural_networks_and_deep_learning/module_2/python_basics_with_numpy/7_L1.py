344eee8b292841b454c91f26056539c65616c1f4
Bradley <bradleymathematics@gmail.com>
2023-07-06 20:07:51 -0400

    add solution

---
+++ neural_networks_and_deep_learning/module_2/python_basics_with_numpy/7_L1.py
@@ -0,0 +1,20 @@
+# GRADED FUNCTION: L1
+
+def L1(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L1 loss function defined above
+    """
+    
+    #(≈ 1 line of code)
+    # loss = 
+    # YOUR CODE STARTS HERE
+    loss = np.linalg.norm(y-yhat, ord=1, axis = 0)
+    
+    # YOUR CODE ENDS HERE
+    
+    return loss
