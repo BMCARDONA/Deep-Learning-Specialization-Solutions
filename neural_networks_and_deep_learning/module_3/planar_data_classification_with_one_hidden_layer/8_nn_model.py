bbf04b97c98163195c59871c760ec18ce745ca1b
Bradley <bradleymathematics@gmail.com>
2023-07-08 14:53:17 -0400

    add solutions

---
+++ neural_networks_and_deep_learning/module_3/planar_data_classification_with_one_hidden_layer/8_nn_model.py
@@ -0,0 +1,61 @@
+# GRADED FUNCTION: nn_model
+
+def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
+    """
+    Arguments:
+    X -- dataset of shape (2, number of examples)
+    Y -- labels of shape (1, number of examples)
+    n_h -- size of the hidden layer
+    num_iterations -- Number of iterations in gradient descent loop
+    print_cost -- if True, print the cost every 1000 iterations
+    
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+    
+    np.random.seed(3)
+    n_x = layer_sizes(X, Y)[0]
+    n_y = layer_sizes(X, Y)[2]
+    
+    # Initialize parameters
+    #(≈ 1 line of code)
+    # parameters = ...
+    
+    # YOUR CODE STARTS HERE
+    # initialize the model's parameters
+    parameters = initialize_parameters(n_x, n_h, n_y)
+    # YOUR CODE ENDS HERE
+    
+    # Loop (gradient descent)
+
+    for i in range(0, num_iterations):
+         
+        #(≈ 4 lines of code)
+        # Forward propagation. Inputs: "X, parameters". Outputs: "A2, cache".
+        # A2, cache = ...
+        
+        # Cost function. Inputs: "A2, Y". Outputs: "cost".
+        # cost = ...
+ 
+        # Backpropagation. Inputs: "parameters, cache, X, Y". Outputs: "grads".
+        # grads = ...
+ 
+        # Gradient descent parameter update. Inputs: "parameters, grads". Outputs: "parameters".
+        # parameters = ...
+        
+        # YOUR CODE STARTS HERE
+        # 1. Implement forward propagation
+        A2, cache = forward_propagation(X, parameters)
+        # 2. Compute loss
+        cost = compute_cost(A2, Y)
+        # 3. Find gradients using backpropagation
+        grads = backward_propagation(parameters, cache, X, Y)
+        # 4. Update parameters using gradient descent
+        parameters = update_parameters(parameters, grads)
+        # YOUR CODE ENDS HERE
+        
+        # Print the cost every 1000 iterations
+        if print_cost and i % 1000 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+
+    return parameters
