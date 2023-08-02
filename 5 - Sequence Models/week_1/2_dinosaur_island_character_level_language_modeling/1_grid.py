d27ed32d74335fd49623a780ce58fada3daf1eab
Bradley <bradleymathematics@gmail.com>
2023-08-02 14:03:26 -0400

    add solutions

---
+++ 5 - Sequence Models/week_1/2_dinosaur_island_character_level_language_modeling/1_grid.py
@@ -0,0 +1,27 @@
+# UNQ_C1 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+### GRADED FUNCTION: clip
+
+def clip(gradients, maxValue):
+    '''
+    Clips the gradients' values between minimum and maximum.
+    
+    Arguments:
+    gradients -- a dictionary containing the gradients "dWaa", "dWax", "dWya", "db", "dby"
+    maxValue -- everything above this number is set to this number, and everything less than -maxValue is set to -maxValue
+    
+    Returns: 
+    gradients -- a dictionary with the clipped gradients.
+    '''
+    gradients = copy.deepcopy(gradients)
+    
+    dWaa, dWax, dWya, db, dby = gradients['dWaa'], gradients['dWax'], gradients['dWya'], gradients['db'], gradients['dby']
+   
+    ### START CODE HERE ###
+    # Clip to mitigate exploding gradients, loop over [dWax, dWaa, dWya, db, dby]. (≈2 lines)
+    for gradient in [dWaa, dWax, dWya, db, dby]:
+        np.clip(gradient, -1 * maxValue, maxValue, out=gradient)
+    ### END CODE HERE ###
+    
+    gradients = {"dWaa": dWaa, "dWax": dWax, "dWya": dWya, "db": db, "dby": dby}
+    
+    return gradients
